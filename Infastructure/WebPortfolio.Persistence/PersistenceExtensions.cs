295d31e17540bff1c5072087a46dc4c01e371802
onurabdulaji <onurabdulaji@gmail.com>
2025-03-15 01:12:30 +0100

    IdentitySeeder PersistenceExtension ConnectionString and Ports

---
+++ Infastructure/WebPortfolio.Persistence/PersistenceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebPortfolio.Domain.Entities;
+using WebPortfolio.Persistence.Context;
+using WebPortfolio.Persistence.Seed;
+
+namespace WebPortfolio.Persistence;
+
+public static class PersistenceExtensions
+{
+    public static void AddPersistenceLayer(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+
+    public static async Task UseIdentityDatabaseSeederAsync(this IServiceProvider service)
+    {
+        using (var scope = service.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+
+            await IdentitySeeder.SeedAsync(userManager, roleManager);
+        }
+    }
+
+}
---
+++ Infastructure/WebPortfolio.Persistence/Seed/IdentitySeeder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebPortfolio.Domain.Entities;
+
+namespace WebPortfolio.Persistence.Seed;
+
+public class IdentitySeeder
+{
+    public static async Task SeedAsync(UserManager<User> userManager, RoleManager<Role> roleManager)
+    {
+        const string adminRoleName = "Admin";
+        const string adminEmail = "onurabdulaji@gmail.com";
+        const string adminPassword = "Admin123!!!"; // Can be pulled from environment variable to be safe.
+
+
+        //Is there an Admin Role? Or Add
+        var roleExists = await roleManager.RoleExistsAsync(adminRoleName);
+        if (!roleExists)
+        {
+            await roleManager.CreateAsync(new Role { Name = adminRoleName });
+        }
+
+        // Is there an Admin User? If not, add
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            adminUser = new User
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                UserFullName = "Onur Abdulaji",
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(adminUser, adminPassword);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, adminRoleName);
+            }
+        }
+    }
+}
